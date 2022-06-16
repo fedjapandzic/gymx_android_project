c1786960f46b41cec1766fa8bd09057bc6a48583
fedjapandzic <feda.pandzic@stu.ibu.edu.ba>
2022-06-16 14:06:09 +0200

    initial commit

---
+++ app/src/main/java/com/example/gymx/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.gymx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialButton signInButton,registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        signInButton = findViewById(R.id.sign_in_button);
+        registerButton = findViewById(R.id.register_button);
+
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+
+}
